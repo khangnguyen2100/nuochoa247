c66747ecabe747dadfe7bfffb9300194b18176f3
khangnguyen2100 <khangnguyen23112100@gmail.com>
2022-10-10 22:07:29 +0700

    reorganization code base

---
+++ index/js/index.js
@@ -0,0 +1,29 @@
+import data from "./data.js";
+const products = document.querySelector(".product__filter");
+const formatPrice = (price) => {
+    return  new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(price)
+}
+const htmls = data.map((product, i) => {
+  if(i < 6) {
+    return `
+    <div class="col-lg-4 col-md-6 col-sm-6">
+    <div class="product__item"> 
+    <div class="product__item__pic set-bg" data-setbg="${product.image}" style='background-image: url(${product.image})' >
+    <ul class="product__hover">
+    <li><a href="#"><img src="img/icon/heart.png" alt=""></a></li>
+    <li><a href="#"><img src="img/icon/compare.png" alt=""> <span>Compare</span></a>
+    </li>
+    <li><a href="#"><img src="img/icon/search.png" alt=""></a></li>
+    </ul>
+    </div>
+    <div class="product__item__text">
+    <h6  style='text-align: center'>${product.name}</h6>
+    <a href="#" class="add-cart">+ Add To Cart</a>
+    <h5  style='text-align: center'>${formatPrice(product.price)}</h5>
+    </div>
+    </div>
+    </div>
+    `;
+  }
+});
+products.innerHTML = htmls.join('')
